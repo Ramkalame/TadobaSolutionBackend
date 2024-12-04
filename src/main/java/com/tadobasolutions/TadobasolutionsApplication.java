540a0fe5a28710a385905e6527d6b0ed0e5594db
Ramkalame <ramkalame14082001@gmail.com>
2024-12-04 15:05:12 +0530

    project is intialized

---
+++ src/main/java/com/tadobasolutions/TadobasolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.tadobasolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadobasolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TadobasolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadobasolutions/TadobasolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadobasolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadobasolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
