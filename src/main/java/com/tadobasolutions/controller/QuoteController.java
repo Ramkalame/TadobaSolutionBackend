ad8f0272683886541f274fe0f70f5a23720ae352
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2024-12-05 10:52:31 +0530

    updated

--- src/main/java/com/tadobasolutions/controller/QuoteController.java
+++ src/main/java/com/tadobasolutions/controller/QuoteController.java
@@ -8,15 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
-@RequestMapping("/quote")
+@RequestMapping("/api/v1/quotes")
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuoteController {
 
     @Autowired
@@ -33,6 +32,19 @@ public class QuoteController {
         response.setSuccess(true);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Quote>>> getAllQuoteList(){
+        List<Quote> data = quoteService.getAllQuoteList();
+        ApiResponse<List<Quote>> response = new ApiResponse<List<Quote>>();
+        response.setData(data);
+        response.setMessage("Quote list fetched successfully!");
+        response.setStatusCode(HttpStatus.CREATED.value());
+        response.setTimeStamp(LocalDate.now());
+        response.setSuccess(true);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
 
 
 
